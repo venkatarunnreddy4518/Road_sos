2429bc03f44df830e6c5372c8a675e5903cf2ff6
venkatarunnreddy4518 <venkatarunnreddy4518@gmail.com>
2026-06-12 13:43:08 +0530

    deploy: update compiled assets with AI Mechanic Assistant on release-web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1392664576" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "228447117" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
