ca5d9d40786377caf2aa9c31eef8f5c1aaecf316
venkatarunnreddy4518 <venkatarunnreddy4518@gmail.com>
2026-06-13 12:17:13 +0530

    feat: implement interactive provider alerts and persistent profile options

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "27417450" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1003936658" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
