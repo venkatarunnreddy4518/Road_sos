e7f15312a4e9cabaec5171143ba13c56660e9aea
venkatarunnreddy4518 <venkatarunnreddy4518@gmail.com>
2026-06-13 09:29:24 +0530

    build: rebuild web files for custom bottom navigation bar

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3904375366" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "723812548" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
